f9726115a81ff472f00893158932aae80eb9e33e
channinghsu <xbc1241888704@gmail.com>
2022-10-24 15:33:58 +0800

    数组04、05

--- src/Array05_Spiral_Matrix_II.java
+++ src/Array05_Spiral_Matrix_II.java
@@ -9,28 +9,34 @@ public class Array05_Spiral_Matrix_II {
         int start = 0;  // 每次循环的开始点(start, start)
         int count = 1;  // 定义填充数字
         int i, j;
+
         while (loop++ < n / 2) { // 判断边界后，loop从1开始
             // 模拟上侧从左到右
             for (j = start; j < n - loop; j++) {
                 res[start][j] = count++;
             }
+
             // 模拟右侧从上到下
             for (i = start; i < n - loop; i++) {
                 res[i][j] = count++;
             }
+
             // 模拟下侧从右到左
             for (; j >= loop; j--) {
                 res[i][j] = count++;
             }
+
             // 模拟左侧从下到上
             for (; i >= loop; i--) {
                 res[i][j] = count++;
             }
             start++;
         }
+
         if (n % 2 == 1) {
             res[start][start] = count;
         }
+
         return res;
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,3 +1,4 @@
+
 public class test {
     public static void main(String[] args) {
         int[] nums ={2,3,1,2,4,3};
@@ -10,7 +11,16 @@ public class test {
 //        Array03_Squares_Of_A_Sorted_Array squaresOfASortedArray = new Array03_Squares_Of_A_Sorted_Array();
 //        System.out.println(Arrays.toString(squaresOfASortedArray.sortedSquares(nums)));
 
-        Array04_Minimum_Size_Subarray_Sum array04 = new Array04_Minimum_Size_Subarray_Sum();
-        System.out.println(array04.minSubArrayLen(nums,7));
+//        Array04_Minimum_Size_Subarray_Sum array04 = new Array04_Minimum_Size_Subarray_Sum();
+//        System.out.println(array04.minSubArrayLen(nums,7));
+        Array05_Spiral_Matrix_II spiral_matrix_ii = new Array05_Spiral_Matrix_II();
+        int n = 5;
+        int[][] mitrax =spiral_matrix_ii.generateMatrix(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mitrax[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
