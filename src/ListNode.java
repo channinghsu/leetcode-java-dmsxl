565cc7564442636f52d45aafc9130658a6a8e151
channinghsu <xbc1241888704@gmail.com>
2022-10-29 10:43:15 +0800

    add comment

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,12 +1,15 @@
-public class ListNode {
+public class ListNode{
     int val;
     ListNode next;
-    ListNode(){}
-    ListNode(int val, ListNode next){
+    // 无参
+    public ListNode(){}
+    // 一个参数
+    public ListNode(int val){
+        this.val = val;
+    }
+    // 二个参数
+    public ListNode(int val, ListNode next){
         this.val = val;
         this.next = next;
-    }
-    ListNode(int val){
-        this.val = val;
     }
 }
