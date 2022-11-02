9ca383fc2e2656af9a7b783cbf0e7a20412f00c1
channinghsu <xbc1241888704@gmail.com>
2022-11-02 11:10:36 +0800

    两个数组的交集

---
+++ src/HashTable02_Intersection_of_Two_Arrays.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+349. 两个数组的交集
+给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+ */
+public class HashTable02_Intersection_of_Two_Arrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+        //遍历数组1
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        //遍历数组2的过程中判断哈希表中是否存在该元素
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                resSet.add(i);
+            }
+        }
+        //将结果几何转为数组
+        return resSet.stream().mapToInt(x -> x).toArray();
+    }
+}
