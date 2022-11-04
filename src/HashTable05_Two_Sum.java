af696ce58a0397b5a75ee243a4d964f4b1739da1
channinghsu <xbc1241888704@gmail.com>
2022-11-04 16:21:56 +0800

    两数之和

---
+++ src/HashTable05_Two_Sum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+/*
+1. 两数之和
+    给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+    你可以按任意顺序返回答案。
+ */
+public class HashTable05_Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if(map.containsKey(temp)){
+                    res[0] = i;
+                    res[1] = map.get(temp);
+                    return res;
+            }
+            map.put(nums[i],i);
+        }
+        return res;
+    }
+
+}
