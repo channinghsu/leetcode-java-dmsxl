81237dcb4f0648bf169003a54b31428c667af8b8
channinghsu <xbc1241888704@gmail.com>
2022-11-09 10:26:05 +0800

    4.两数之和

--- src/HashTable04_Two_Sum.java
+++ src/HashTable04_Two_Sum.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
     你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
     你可以按任意顺序返回答案。
  */
-public class HashTable05_Two_Sum {
+public class HashTable04_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         HashMap<Integer,Integer> map = new HashMap<>();
