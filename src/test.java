a370ac243e6d7e307b5cc79eab02673c9492eb72
channinghsu <xbc1241888704@gmail.com>
2023-01-16 22:45:48 +0800

    macbook pro 14 IDEA Git test

--- src/test.java
+++ src/test.java
@@ -1,11 +1,5 @@
 public class test {
     public static void main(String[] args) {
-//        LinkedList02_Design_Linked_List design = new LinkedList02_Design_Linked_List();
-//        design.addAtHead(2);
-//        design.addAtHead(3);
-//        design.addAtHead(4);
-//        LinkedList05_Remove_Nth_Node_From_End_of_List remove = new LinkedList05_Remove_Nth_Node_From_End_of_List();
-//        System.out.println(remove.removeNthFromEnd(design.dummyHead,2));
 
         HashTable02_Intersection_of_Two_Arrays inter = new HashTable02_Intersection_of_Two_Arrays();
         int[] a = {1,2,3,4,2};
